88e7baf352d252c23b031ba918fb79f43ed4d7e3
Preda Bogdan <bogdaWK@gmail.com>
2018-07-20 00:32:31 +0300

    chore: improved styles and added startup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* @flow */
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const files = require('./files');
+
+clear();
+console.log(
+	chalk.yellow(
+		figlet.textSync( 'ThemeIsle CLI', {horizontalLayout: 'full'} )
+	)
+);
+
+if ( files.directoryExists( '.git' ) ) {
+	console.log( chalk.red( 'Already a git repository!' ) );
+	process.exit();
+}
